c925b2b6380d8c0901e03efda0d647200650977c
oocjh12 <74766182+oocjh12@users.noreply.github.com>
2021-01-11 10:43:01 +0900

    youtubeAPI
    영상가져오기

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -15,38 +15,25 @@ def home():
 
 
 # API 역할을 하는 부분
-@app.route('/api/list', methods=['GET'])
-def show_cardioVideos():
-    # 1. db에서 mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
-    # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
-    stars = list(db.mystar.find({}, {'_id': False}).sort('like', -1))
-    # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
-    return jsonify({'result': 'success', 'stars_list': stars})
+@app.route('/stretch/fullbody', methods=['GET'])
+def show_fullbodyST():
+    fullbodySTs = list(db.myproject.find({}, {"_id": 0, "videoId": 1, "subCAT": "fullbodyST"}).limit(4))
+    # fullbodySTs = list(db.myproject.find({}, {"subCAT": "fullbodyST"}).sort([{"_id": 1}]))
+    # fullbodySTs = list(db.myproject.find({"subCAT": "fullbodyST"},{'videoId':1}))
+    return jsonify({'result': 'success', 'fullbodyST_list': fullbodySTs})
 
 
-# @app.route('/api/like', methods=['POST'])
-# def like_star():
-#     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-#     name_receive = request.form['name_give']
-#     # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
-#     star = db.mystar.find_one({'name': name_receive})
-#     # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
-#     new_like = star['like'] + 1
-#     # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
-#     # 참고: '$set' 활용하기!
-#     db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
-#     # 5. 성공하면 success 메시지를 반환합니다.
-#     return jsonify({'result': 'success'})
-#
-#
-# @app.route('/api/delete', methods=['POST'])
-# def delete_star():
-#     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-#     name_receive = request.form['name_give']
-#     # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
-#     db.mystar.delete_one({'name': name_receive})
-#     # 3. 성공하면 success 메시지를 반환합니다.
-#     return jsonify({'result': 'success'})
+@app.route('/stretch/upperbody', methods=['GET'])
+def show_upperbodyST():
+    upperbodySTs = list(db.myproject.find({}, {"_id": 0, "videoId": 1, "subCAT": "upperbodyST"}).limit(4))
+    return jsonify({'result': 'success', 'upperbodyST_list': upperbodySTs})
+
+
+@app.route('/stretch/lowerbody', methods=['GET'])
+def show_lowerbodyST():
+    lowerbodySTs = list(db.myproject.find({}, {"_id": 0, "videoId": 1, "subCAT": "lowerbodyST"}).limit(4))
+    return jsonify({'result': 'success', 'lowerbodyST_list': lowerbodySTs})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
