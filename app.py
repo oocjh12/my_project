6528f8ac18b27a3935288b9ae0ae17586b244a34
oocjh12 <74766182+oocjh12@users.noreply.github.com>
2021-01-19 23:14:26 +0900

    YoutubeAPI_app.py(pagination)
    영상 8개씩 가져오도록 수정

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import math
+
 from pymongo import MongoClient
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -15,94 +17,153 @@ def home():
 
 
 # API 역할을 하는 부분
-
-# @app.route('/st/fullbody', methods=['GET'])
-# def show_fullbodyST():
-#     fullbodySTs = list(db.myproject.find({}, {"subCAT": "fullbodyST", "_id": 0, "videoId": 1}).limit(8))
-#     # fullbodySTs = list(db.myproject.find({}, {"subCAT": "fullbodyST"}).sort([{"_id": 1}]))
-#     # fullbodySTs = list(db.myproject.find({"subCAT": "fullbodyST"},{'videoId':1}))
-#     return jsonify({'result': 'success', 'fullbodyST_list': fullbodySTs})
-#
-#
-# @app.route('/st/upperbody', methods=['GET'])
-# def show_upperbodyST():
-#     upperbodySTs = list(db.myproject.find({}, {"subCAT": "upperbodyST", "_id": 0, "videoId": 1}).limit(8))
-#     return jsonify({'result': 'success', 'upperbodyST_list': upperbodySTs})
-#
-#
-# @app.route('/st/lowerbody', methods=['GET'])
-# def show_lowerbodyST():
-#     lowerbodySTs = list(db.myproject.find({}, {"subCAT": "lowerbodyST", "_id": 0, "videoId": 1}).limit(8))
-#     return jsonify({'result': 'success', 'lowerbodyST_list': lowerbodySTs})
-
 # 스트레칭 API
 @app.route('/st/fullbody', methods=['GET'])
 def show_fullbodyST():
-    fullbody_sts = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'fullbodyST'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    fullbody_sts = list(db.myproject.find({'subCAT': 'fullbodyST'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'fullbodyST_list': fullbody_sts})
-    # fullbodySTs = list(db.myproject.find({}, {"subCAT": "fullbodyST"}).sort([{"_id": 1}]))
-    # fullbodySTs = list(db.myproject.find({"subCAT": "fullbodyST"},{'videoId':1}))
 
 
 @app.route('/st/upperbody', methods=['GET'])
 def show_upperbodyST():
-    upperbody_sts = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'upperbodyST'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    upperbody_sts = list(db.myproject.find({'subCAT': 'upperbodyST'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'upperbodyST_list': upperbody_sts})
-    # for upperbody_st in upperbody_sts:
-    #     print(upperbody_st)
 
 
 @app.route('/st/lowerbody', methods=['GET'])
 def show_lowerbodyST():
-    lowerbody_sts = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'lowerbodyST'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    lowerbody_sts = list(db.myproject.find({'subCAT': 'lowerbodyST'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'lowerbodyST_list': lowerbody_sts})
 
 
 # 유산소 운동 API
 @app.route('/cardio', methods=['GET'])
 def show_cardio():
-    cardios = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'Cardio'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    cardios = list(db.myproject.find({'subCAT': 'Cardio'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'cardio_list': cardios})
 
 
 # 근력 운동 API
 @app.route('/w/upperbody', methods=['GET'])
 def show_upperbodyW():
-    upperbody_ws = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'upperbodyW'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    upperbody_ws = list(db.myproject.find({'subCAT': 'upperbodyW'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'upperbodyW_list': upperbody_ws})
 
 
 @app.route('/w/lowerbody', methods=['GET'])
 def show_lowerbodyW():
-    lowerbody_ws = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'lowerbodyW'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    lowerbody_ws = list(db.myproject.find({'subCAT': 'lowerbodyW'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'lowerbodyW_list': lowerbody_ws})
 
 
 @app.route('/w/core', methods=['GET'])
 def show_coreW():
-    core_ws = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'coreW'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    core_ws = list(db.myproject.find({'subCAT': 'coreW'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'coreW_list': core_ws})
 
 
 @app.route('/w/abs', methods=['GET'])
 def show_absW():
-    abs_ws = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'absW'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    abs_ws = list(db.myproject.find({'subCAT': 'absW'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'absW_list': abs_ws})
 
 
 @app.route('/w/hip', methods=['GET'])
 def show_hipW():
-    hip_ws = list(
-        db.myproject.aggregate([{'$match': {'subCAT': 'hipW'}}, {'$project': {'_id': 0}}]))
+    page = int(request.args.get('page', 1))
+    LIMIT = 8
+    offset = (page - 1) * LIMIT
+    hip_ws = list(db.myproject.find({'subCAT': 'hipW'}, {'_id': 0}).limit(LIMIT).skip(offset))
     return jsonify({'result': 'success', 'hipW_list': hip_ws})
+
+
+# @app.route('/st/fullbody', methods=['GET'])
+# def show_fullbodyST():
+#     fullbody_sts = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'fullbodyST'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'fullbodyST_list': fullbody_sts})
+
+
+# @app.route('/st/upperbody', methods=['GET'])
+# def show_upperbodyST():
+#     upperbody_sts = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'upperbodyST'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'upperbodyST_list': upperbody_sts})
+
+
+# @app.route('/st/lowerbody', methods=['GET'])
+# def show_lowerbodyST():
+#     lowerbody_sts = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'lowerbodyST'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'lowerbodyST_list': lowerbody_sts})
+
+
+# 유산소 운동 API
+# @app.route('/cardio', methods=['GET'])
+# def show_cardio():
+#     cardios = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'Cardio'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'cardio_list': cardios})
+
+
+# 근력 운동 API
+# @app.route('/w/upperbody', methods=['GET'])
+# def show_upperbodyW():
+#     upperbody_ws = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'upperbodyW'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'upperbodyW_list': upperbody_ws})
+
+
+# @app.route('/w/lowerbody', methods=['GET'])
+# def show_lowerbodyW():
+#     lowerbody_ws = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'lowerbodyW'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'lowerbodyW_list': lowerbody_ws})
+
+
+# @app.route('/w/core', methods=['GET'])
+# def show_coreW():
+#     core_ws = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'coreW'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'coreW_list': core_ws})
+#
+#
+# @app.route('/w/abs', methods=['GET'])
+# def show_absW():
+#     abs_ws = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'absW'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'absW_list': abs_ws})
+#
+#
+# @app.route('/w/hip', methods=['GET'])
+# def show_hipW():
+#     hip_ws = list(
+#         db.myproject.aggregate([{'$match': {'subCAT': 'hipW'}}, {'$project': {'_id': 0}}]))
+#     return jsonify({'result': 'success', 'hipW_list': hip_ws})
 
 
 if __name__ == '__main__':
--- init_db.py
+++ init_db.py
@@ -4,6 +4,9 @@ from oauth2client.tools import argparser
 from pymongo import MongoClient
 import json
 
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
@@ -11,7 +14,7 @@ db = client.dbsparta
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = ""
+DEVELOPER_KEY = config['DEVELOPER_KEY']
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
@@ -22,7 +25,7 @@ def youtube_search(options):
 
     # Call the search.list method to retrieve results matching the specified
     # query term.
-    searchWord = '엉덩이운동'
+    searchWord = '전신스트레칭'
     mainCode = {'전신스트레칭': 'Stretch', '상체스트레칭': 'Stretch', '하체스트레칭': 'Stretch', '유산소운동': 'Cardio', '상체근력운동': 'Weight',
                 '하체근력운동': 'Weight', '코어근력운동': 'Weight', '복근운동': 'Weight', '엉덩이운동': 'Weight'}
     subCode = {'전신스트레칭': 'fullbodyST', '상체스트레칭': 'upperbodyST', '하체스트레칭': 'lowerbodyST', '유산소운동': 'Cardio',
@@ -30,7 +33,7 @@ def youtube_search(options):
     search_response = youtube.search().list(
         q=searchWord,  # 변수 처리
         part="snippet",
-        maxResults=50
+        maxResults=10
     ).execute()
 
     # Add each result to the appropriate list, and then display the lists of
